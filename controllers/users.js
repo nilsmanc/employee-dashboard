9b0ae58210ad7b38f48bf1c0f27b2f9e181faf18
nilsmanc <etereaiberia@gmail.com>
2023-06-19 08:22:46 +0300

    sign up

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,11 @@
 const { prisma } = require('../prisma/prisma-client')
-const bcrypt = require('bcrypt')
+const brypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const login = async (req, res) => {
   const { email, password } = req.body
 
-  if (!email && !password) {
+  if (!email || !password) {
     return res
       .status(400)
       .json({ message: 'Please fill in the required fields' })
@@ -18,7 +18,7 @@ const login = async (req, res) => {
   })
 
   const isPasswordCorrect =
-    user && (await bcrypt.compare(password, user.password))
+    user && (await brypt.compare(password, user.password))
 
   if (user && isPasswordCorrect) {
     res.status(200).json({
@@ -31,8 +31,56 @@ const login = async (req, res) => {
   }
 }
 
+/**
+ *
+ * @route POST /api/user/register
+ * @desc Sign up
+ * @access Public
+ *
+ */
+
 const register = async (req, res) => {
-  res.send('register')
+  const { email, password, name } = req.body
+
+  if (!email || !password || !name) {
+    return res.send(400).json({ message: 'Please fill in the required fields' })
+  }
+
+  const registeredUser = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  })
+
+  if (registeredUser) {
+    return res
+      .status(400)
+      .json({ message: 'User with that email already exists' })
+  }
+
+  const salt = await brypt.genSalt(10)
+  const hashedPassword = await brypt.hash(password, salt)
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  })
+
+  const secret = process.env.JWT_SECRET
+
+  if (user && secret) {
+    res.status(201).json({
+      id: user.id,
+      email: user.email,
+      name,
+      token: jwt.sign({ id: user.id }, secret, { expiresIn: '30d' }),
+    })
+  } else {
+    return res.status(400).json({ message: 'Failed to create user' })
+  }
 }
 
 const current = async (req, res) => {
