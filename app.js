52123c74e2db5a2ddc92ca3410af89fc5b5287a2
nilsmanc <etereaiberia@gmail.com>
2023-06-21 08:00:03 +0300

    employee routes

--- app.js
+++ app.js
@@ -13,5 +13,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/api/user', require('./routes/users'))
+app.use('/api/employee', require('./routes/employees'))
 
 module.exports = app
---
+++ routes/employees.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = require('router')
+const { auth } = require('../middleware/auth')
+
+// /api/employees
+router.get('/', auth, () => console.log('get all employees'))
+
+// /api/employees/:id
+router.get('/:id', auth, () => console.log('get one employee'))
+
+// /api/employees/add
+router.post('/add', auth, () => console.log('add employee'))
+
+// /api/employees/remove/:id
+router.post('/remove/:id', auth, () => console.log('remove employee'))
+
+// /api/employees/edit/:id
+router.put('/edit/:id', auth, () => console.log('edit employee'))
+
+module.exports = router
